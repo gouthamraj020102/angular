ede4b79c1d5bf1344cc94455a59b2c6ed2e71f22
gouthamraj020102 <gouthamraj020102@gmail.com>
2025-11-03 15:04:29 +0530

    Utilized data model in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ];
   title = 'angular';
 }
